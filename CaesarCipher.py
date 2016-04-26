75b6a91333b2b402a4c5898b13d299dab42a4e58
muslumoncel <muslumoncel10@gmail.com>
2016-04-26 19:12:11 +0300

    com

---
+++ CaesarCipher.py
@@ -0,0 +1,19 @@
+N = int(input().strip())
+sentence = input().strip()
+K = int(input().strip())
+message = ""
+for char in sentence:
+    temp = int(ord(char))
+    temp1 = K
+    if 64 < ord(char) < 91 or 96 < ord(char) < 123:
+        while temp1 > 0:
+            temp += 1
+            if temp == 91:
+                temp = 65
+            if temp == 123:
+                temp = 97
+            temp1 -= 1
+        message += chr(temp)
+    else:
+        message += char
+print(message)
