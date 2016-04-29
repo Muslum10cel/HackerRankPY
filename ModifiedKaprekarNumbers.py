9e4901dd79ac6e528868246c733d519577ff6562
muslumoncel <muslumoncel10@gmail.com>
2016-04-29 17:45:57 +0300

    commit

--- ModifiedKaprekarNumbers.py
+++ ModifiedKaprekarNumbers.py
@@ -4,6 +4,7 @@ found = 0
 for i in range(strt, end + 1):
     if i == 1:
         print(1, end=" ")
+        continue
     temp = str(i ** 2)
     try:
         if int(temp[:len(temp) // 2]) + int(temp[len(temp) // 2:]) == i:
