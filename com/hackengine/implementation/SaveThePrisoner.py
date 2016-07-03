fabf7615bec90118426c4f90832f5b705489c5dc
muslumoncel <muslumoncel10@gmail.com>
2016-07-03 16:10:10 +0300

    commit

--- com/hackengine/implementation/SaveThePrisoner.py
+++ com/hackengine/implementation/SaveThePrisoner.py
@@ -1,14 +1,13 @@
 def find(m, n, s):
     if (m + s) <= n:
         return m + s
-    else:
-        return (m + s) - n
+    return (m + s) - n
 
 
 T = int(input().strip())
 while T > 0:
-    NMS = input().split(" ")
-    N, M, S = int(NMS[0]), int(NMS[1]) - 1, int(NMS[2])
+    N, M, S = map(int, input().strip().split(" "))
+    M -= 1
     if M > N:
         M %= N
         print(find(M, N, S))
