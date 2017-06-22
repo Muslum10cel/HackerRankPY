f88e4e2eff900515564abcab40de1749e77e249e
muslumoncel <muslumoncel10@gmail.com>
2017-06-22 18:26:36 +0300

    commit

--- com/hackengine/implementation/AppleAndOrange.py
+++ com/hackengine/implementation/AppleAndOrange.py
@@ -1,16 +1,10 @@
+def f(fruits, s, t):
+    return sum(map(lambda x, y=0: y + 1 if s <= x <= t else y, fruits))
+
+
 s, t = map(int, input().split(" "))
 a, b = map(int, input().split(" "))
 m, n = map(int, input().split(" "))
 APPLES = [int(x) + a for x in input().split(" ")]
 ORANGES = [int(x) + b for x in input().split(" ")]
-appleCount, orangeCount = 0, 0
-
-for apple in APPLES:
-    if s <= apple <= t:
-        appleCount += 1
-for orange in ORANGES:
-    if s <= orange <= t:
-        orangeCount += 1
-
-print(appleCount)
-print(orangeCount)
+print(f(APPLES, s, t), f(ORANGES, s, t), sep="\n")
