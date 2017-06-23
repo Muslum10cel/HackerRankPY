b7747710cae2c58e77351b90828f72f4e815a557
muslumoncel <muslumoncel10@gmail.com>
2017-06-24 00:13:01 +0300

    commit

--- com/hackengine/implementation/DesignerPDFViewer.py
+++ com/hackengine/implementation/DesignerPDFViewer.py
@@ -1,6 +1,3 @@
 heights = [int(x) for x in input().split(" ")]
 word = input()
-ls = []
-for char in word:
-    ls.append(heights[ord(char) - 97])
-print(max(ls) * len(word))
+print(max(map(lambda x: heights[ord(x) - 97], word)) * len(word))
