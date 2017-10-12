405b182670a542eca0ba532861201741946007db
muslum10cel <muslumoncel10@gmail.com>
2017-10-13 00:05:48 +0300

    commit

--- com/hackengine/strings/MarsExploration.py
+++ com/hackengine/strings/MarsExploration.py
@@ -1,7 +1,3 @@
 s = input()
 t = len(s) // 3 * "SOS"
-count = 0
-for i in range(0, len(s)):
-    if s[i] != t[i]:
-        count += 1
-print(count)
+print(len(list(filter(lambda x: x > 0, [ord(a) ^ ord(b) for a, b in zip(s, t)]))))
