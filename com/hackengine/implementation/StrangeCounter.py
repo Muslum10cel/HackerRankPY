9e0c58be02c33166c78048b18aa5f7a90e0a7bd1
muslum10cel <muslumoncel10@gmail.com>
2017-11-08 21:26:36 +0300

    commit

--- com/hackengine/implementation/StrangeCounter.py
+++ com/hackengine/implementation/StrangeCounter.py
@@ -2,17 +2,15 @@ import math as m
 
 
 def result(t):
+    n = m.floor((m.log10((t + 3) / 3)) / (m.log10(2)))
+    temp = 3 * (2 ** n) - 3
     if t == 1:
         return 3
     elif t == 2:
         return 2
-    elif t == 3:
+    elif t == temp:
         return 1
     else:
-        n = m.floor((m.log10((t + 3) / 3)) / (m.log10(2)))
-        temp = 3 * (2 ** n) - 3
-        if t == temp:
-            return 1
         tcycbeg = temp + 1
         tvalbeg = 3 * (2 ** n)
         return tvalbeg - (abs(tcycbeg - t))
