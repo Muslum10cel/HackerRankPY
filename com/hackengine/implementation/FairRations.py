aac9d02ef22952d5116c1d6c3c5aa5e7c4295a1e
muslum10cel <muslumoncel10@gmail.com>
2017-11-21 19:28:45 +0300

    commit

---
+++ com/hackengine/implementation/FairRations.py
@@ -0,0 +1,19 @@
+def solution(loaves):
+    bread = 0
+    length = len(loaves)
+
+    for i in range(length - 1):
+        if loaves[i] % 2 != 0:
+            loaves[i + 1] += 1
+            loaves[i] += 1
+            bread += 2
+    if sum(loaves) % 2 == 0:
+        print(bread)
+    else:
+        print("NO")
+
+
+if __name__ == "__main__":
+    br = int(input())
+    loaf = [int(x) for x in input().split(" ")]
+    solution(loaf)
